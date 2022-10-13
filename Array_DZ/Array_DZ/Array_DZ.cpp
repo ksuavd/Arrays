477819f39780b0289326fb4ebdf694d6bc0f85b0
Avdee_nw28 <Avdee_nw28@itstepru.onmicrosoft.com>
2022-10-13 12:06:39 +0500

    Array_dz

---
+++ Array_DZ/Array_DZ/Array_DZ.cpp
@@ -0,0 +1,48 @@
+﻿// Array_DZ
+
+#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	const int Size = 5;
+	int Array[Size] = { };
+	cout << "Введите " << 0 << " элемент массива: "; cin >> Array[0];
+	cout << "Введите " << 1 << " элемент массива: "; cin >> Array[1];
+	cout << "Введите " << 2 << " элемент массива: "; cin >> Array[2];
+	cout << "Введите " << 3 << " элемент массива: "; cin >> Array[3];
+	cout << "Введите " << 4 << " элемент массива: "; cin >> Array[4];
+	Sleep(700);
+	system("cls");
+	cout << "Ваш массив: ";
+	for (int i = 0; i < Size; i++)
+	{
+		cout << Array[i] << " ";
+	}
+	cout << endl;
+
+	cout << "Массив в обратном порядке: ";
+	for (int i = Size - 1; i >= 0; i--)
+	{
+		cout << Array[i] << " ";
+	}
+	cout << endl;
+
+	int Summa = 0;
+	for (int i = 0; i < Size; i++)
+	{
+		Summa = Summa + Array[i];
+	}
+	cout << "Сумма элементов массива: " << Summa; cout << endl;
+	cout << "Среднее-арифметическое массива: " << Summa / Size; cout << endl;
+	int Min = Array[0]; int Max = Array[0];
+	for (int i = 0; i < Size; i++)
+	{
+		if (Min > Array[i]) Min = Array[i];
+		if (Max < Array[i]) Max = Array[i];
+	}
+	cout << "Минимальное число массива: " << Min << "\n" << "Максимальное число массива: " << Max; cout << endl;
+}
